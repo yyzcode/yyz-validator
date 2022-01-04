12043b0d0069af02aa7d980ac827175ac36b0b54
李万元 <449736038@qq.com>
2022-01-04 10:35:57 +0800

    chore: 补全常用基础类型的验证器

--- validator.go
+++ validator.go
@@ -34,6 +34,83 @@ func Int(value int, field string) *intValidator {
 	}
 }
 
+func Uint(value uint, field string) *uintValidator {
+	return &uintValidator{
+		field: field,
+		value: value,
+	}
+}
+
+func Int8(value int8, field string) *int8Validator {
+	return &int8Validator{
+		field: field,
+		value: value,
+	}
+}
+
+func Uint8(value uint8, field string) *uint8Validator {
+	return &uint8Validator{
+		field: field,
+		value: value,
+	}
+}
+
+func Int16(value int16, field string) *int16Validator {
+	return &int16Validator{
+		field: field,
+		value: value,
+	}
+}
+
+func Uint16(value uint16, field string) *uint16Validator {
+	return &uint16Validator{
+		field: field,
+		value: value,
+	}
+}
+
+func Int32(value int32, field string) *int32Validator {
+	return &int32Validator{
+		field: field,
+		value: value,
+	}
+}
+
+func Uint32(value uint32, field string) *uint32Validator {
+	return &uint32Validator{
+		field: field,
+		value: value,
+	}
+}
+
+func Int64(value int64, field string) *int64Validator {
+	return &int64Validator{
+		field: field,
+		value: value,
+	}
+}
+
+func Uint64(value uint64, field string) *uint64Validator {
+	return &uint64Validator{
+		field: field,
+		value: value,
+	}
+}
+
+func Float32(value float32, field string) *float32Validator {
+	return &float32Validator{
+		field: field,
+		value: value,
+	}
+}
+
+func Float64(value float64, field string) *float64Validator {
+	return &float64Validator{
+		field: field,
+		value: value,
+	}
+}
+
 func String(value string, field string) *stringValidator {
 	return &stringValidator{
 		field: field,
